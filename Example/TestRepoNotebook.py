2f3e1c384ae30306c31e481e94955bcb32097e7a
SrividyaPeddireddy <SrividyaPeddireddy>
2022-11-17 06:45:08 +0000

    added notebook1

---
+++ Example/TestRepoNotebook.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC SELECT "hello world"
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "I'm running sql"
+
+# COMMAND ----------
+
+print("Hello World")
---
+++ Example/testNotebook1.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+print("hello world")
+
+# COMMAND ----------
+
+region = "nyc"
+store = "100"
+year = "2021"
+
+print(f"{region}+{store}+_sales_+{year}")
+print("{region}+{store}+_sales_+{year}")
+print(f"{region}{store}_sales_{year}")
